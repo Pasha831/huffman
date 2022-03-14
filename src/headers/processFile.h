437740bd54149d1616fdf053b76058a0b8b1819e
Pavel Medvedev <pashamedvedev03@gmail.com>
2022-03-14 17:44:51 +0300

    created insertion of a path of a file, creation of a new compressed folder with a compressed file next to input file, and other fixes :)

--- src/headers/processFile.h
+++ src/headers/processFile.h
@@ -3,7 +3,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <time.h>
 
+#define MAX_FILE_SIZE 1000
+
+typedef struct File {
+    FILE *f;
+    char fileLocation[MAX_FILE_SIZE];
+    char fileName[MAX_FILE_SIZE];
+    char fileExtension[MAX_FILE_SIZE];
+    char rootFolder[MAX_FILE_SIZE];
+    unsigned long long fileLength;
+} File;
+
+void showExecutionTime(const clock_t *start, const clock_t *end);
+File initInputFile();
+File initOutputFile(File *inputFile);
+void getFileInfo(char *fileLocation, char *rootFolder, char *fileExtension, char *fileName);
 FILE *openFile(char *fileName, unsigned long long *length);
 void fillDictionary(FILE *fr, const unsigned long long *length, int *freq);
 
