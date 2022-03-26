f778de98f2690b80a5148464f0437778ee9dc9ca
maxveega <max-im.ageev@yandex.ru>
2022-03-26 12:38:45 +0300

    finally creating meta

--- src/headers/decoding.h
+++ src/headers/decoding.h
@@ -5,4 +5,6 @@
 #define HUFFMAN_DECODING_H
 
 void decodeFile(FILE *in, FILE *out, Node *root);
+Node* metaToTree (FILE* meta);
+
 #endif //HUFFMAN_DECODING_H
--- src/source/main.c
+++ src/source/main.c
@@ -5,24 +5,33 @@
 
 int main() {
     File inputFile = initInputFile();
+    File outputFile = initOutputFile(&inputFile);
+
     int freq[ASCII_COUNT] = { 0 };  // counter for each ascii element of file
     clock_t start = clock();  // just to see, how long it takes to encode & decode file
 
     fillDictionary(inputFile.f, &inputFile.fileLength, freq);
 
     Node *list = createList(freq);
+    createMeta(&outputFile, list);
     list = makeTreeFromList(list);
 
     char dict[ASCII_COUNT][ASCII_COUNT] = { 0 };
     calculateHuffmanCodes(list, dict);
 
-    File outputFile = initOutputFile(&inputFile);
     encodeFile(inputFile.f, outputFile.f, &inputFile.fileLength, dict);
     fclose(outputFile.f);
 
     FILE *encoded = fopen(outputFile.fileLocation, "rb");
     FILE *decoded = fopen("new.txt", "wb");
-    decodeFile(encoded, decoded, list);
+
+    char metaName[MAX_FILE_SIZE] = { 0 };
+    strcpy(metaName, outputFile.rootFolder);
+    strcat(metaName, "meta.txt");
+
+    FILE *meta = fopen(metaName, "rt");
+    Node* tree = metaToTree(meta);
+    decodeFile(encoded, decoded, tree);
 
     showHappyEnd();
     clock_t end = clock();
